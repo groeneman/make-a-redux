8de581232a73a5841f48286af322dba2f2a93300
Michael Groeneman <michael@groeneman.com>
2017-04-12 12:04:25 -0700

    Add AddTodo

---
+++ src/AddTodo.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddTodo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { name: '' }
+    this.onInputChange = this.onInputChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onInputChange(event) {
+    this.setState({ name: event.target.value })
+  }
+
+  onSubmit(event) {
+    event.preventDefault()
+    this.props.dispatch(/* something goes here */)
+  }
+
+  render() {
+    return (
+      <form onSubmit={ this.onSubmit }>
+        <input type="text" value={ this.state.name } onChange={ this.onInputChange } />
+        <button type="submit">Add</button>
+      </form>
+    );
+  }
+}
+
+AddTodo.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+}
+
+export default AddTodo;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoList from './TodoList.js'
+import AddTodo from './AddTodo.js'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <TodoList todos={ [{ id: 1, name: 'Make a redux clone' }] } />
+        <AddTodo />
       </div>
     );
   }
